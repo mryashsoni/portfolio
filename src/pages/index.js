aaafcc62692f070414d4c0b9082d420896dff8ab
mryashsoni <42246474+mryashsoni@users.noreply.github.com>
2021-02-23 14:45:16 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Yash Soni'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Yash Soni'} />
       </Helmet>
       <App />
     </>
